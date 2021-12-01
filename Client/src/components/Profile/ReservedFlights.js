536729f3442d700a75972bafc80a7c97bd3cbca6
Zeyad ElSaeed <ziadelnagar380@gmail.com>
2021-12-01 23:34:43 +0200

    push package

--- Client/src/components/Profile/ReservedFlights.js
+++ Client/src/components/Profile/ReservedFlights.js
@@ -8,39 +8,47 @@ const ReservedFlights = () => {
     const [ flights , setFlights ] = React.useState();
 
     React.useEffect( async() => {
+        console.log(15);
         const user = {
-         username : 'konar'
-        };
+            username: "konar"
+         };
+        console.log(1);
+
          await axios.post('http://localhost:8000/user/viewMyReservations', user)
              .then(result => {
                setFlights(result.data);
                console.log(flights);
                console.log(result.data);
              }).catch(err => {
-                alert("Error with The Server "+ err );
+                console.log(5);
+                alert("Error with The Server " + err );
+                
+
            });
+           console.log(0);
+
 
        }, [] );
 
     return (
         <div>
 
-{ 
-    flights.map((oneElement) =>
-    <Grid key={oneElement.flightNumber}  item xs={4} sx={{minWidth: "450px"}}>
+{/* { 
+//     flights.map((oneElement) =>
+//     <Grid key={oneElement.flightNumber}  item xs={4} sx={{minWidth: "450px"}}>
 
-    <Ticket 
-   passengerName = "konar"
-   departureFlight = {oneElement.departureFlight}
-   departureSeats={oneElement.departureSeats}
-   returnFlight ={oneElement.returnFlight}
-   returnSeats ={oneElement.returnSeats}
-   cabinClass ={oneElement.cabinClass}
-   />
-    </Grid>
+//     <Ticket 
+//    passengerName = "konar"
+//    departureFlight = {oneElement.departureFlight}
+//    departureSeats={oneElement.departureSeats}
+//    returnFlight ={oneElement.returnFlight}
+//    returnSeats ={oneElement.returnSeats}
+//    cabinClass ={oneElement.cabinClass}
+//    />
+//     </Grid>
 
 
- )}
+ )} */}
 
             
         </div>
