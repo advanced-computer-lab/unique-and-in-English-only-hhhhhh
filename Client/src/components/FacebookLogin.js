12775d299eaae6b95116e38ffffd9717df370b43
alihelmy27 <alihelmy6@gmail.com>
2021-12-25 23:02:34 +0200

    facebook updated

--- Client/src/components/FacebookLogin.js
+++ Client/src/components/FacebookLogin.js
@@ -2,6 +2,9 @@ import React,{ useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
 import { Card, Image } from 'react-bootstrap';
 import { Redirect } from 'react-router';
+import axios from 'axios';
+import App from '../App';
+import Link from '@mui/material/Link';
 
 function Facebook() {
   const [username,setUsername] = React.useState("")
@@ -10,26 +13,72 @@ function Facebook() {
   const [picture, setPicture] = useState('');
   const [logged , setLogged ] = React.useState( false );
   const [finish, setFinish] = React.useState(false);
+  const [error , setError] = React.useState(false);
+  const [message , setMessage] = React.useState({isVisible: false , message: "I am here"});
+  const [password,setPassword] = React.useState("");
+  var isFound=false;
 
-  const responseFacebook = (response) => {
+  const responseFacebook = async function(response){
     console.log(response);
     setData(response);
-    console.log(response.name);
-    setPicture(response.picture.data.url);
-
-      localStorage.setItem('username', response.name);
-      localStorage.setItem('user token', response.accessToken);
-      localStorage.setItem('type', "User");
-      setLogged(true);
-      setUsername(response.name);
-      window.setTimeout( () => {setFinish(true)}, 5000);
-     
+    var fullname=response.name.split(" ");
+    setPicture(response.picture.data.url);  
     if (response.accessToken) {
       setLogin(true);
-    } else {
+      const user = {
+        userName:   response.name ,
+        password:   "12345"
+      }
+      await axios.post('http://localhost:8000/user/login' , user)
+      .then(res => {
+        console.log(res);
+        if(res.data.message == "Success"){
+          localStorage.setItem('username', response.name);
+          localStorage.setItem('user token',response.accessToken);
+          localStorage.setItem('type', "User");
+          setLogged(true);
+          setUsername(response.name);
+          setFinish(true);
+          isFound=true;
+        }
+          
+      }).catch(err => {
+        alert("Connection Error with the server");
+    });
+    if(!isFound){
+      const newUser = {
+        firstName: fullname[0],
+        lastName: fullname[1],
+        email: fullname[0]+fullname[1]+"facebook@gmail.com",
+        userName: response.name,
+        password: "12345",
+        gender:"",
+        country:"",
+        telephoneNumber:"",
+        dateOfBirth: "",
+        passportNumber: "diuwhfiuewf"
+      };
+      console.log(newUser);
+      await axios.post('http://localhost:8000/guest/sign-up' , newUser)
+      .then((result) => {
+        console.log(result.data.message);
+        if(result.data.message == "Success"){
+        localStorage.setItem('username', response.name);
+        localStorage.setItem('user token', response.accessToken);
+        localStorage.setItem('type', "User");
+          setLogged(true);
+          setUsername(response.name);
+          setFinish(true);
+        }
+      }).catch(err => {
+        console.log(err);
+    });}
+  }
+   else {
       setLogin(false);
     }
   }
+
   if ( logged == true ) {
 
     return (
